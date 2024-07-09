315a92a73de38267f7a79dbdacdb2177db9efb4c
Sabeur DAMAK <sabeur.damak@continental-corporation.com>
2024-07-09 11:01:43 +0200

    add tcp

--- doip.py
+++ doip.py
@@ -1,6 +1,6 @@
 from doipclient import DoIPClient
-# ~ print("<<<<<<<<<<<<<<<<<<<<<<<<<<<<")
-# ~ print("await_vehicle_announcement")
+print("<<<<<<<<<<<<<<<<<<<<<<<<<<<<")
+print("await_vehicle_announcement")
 # ~ address, announcement = DoIPClient.await_vehicle_announcement()
 
 # ~ # Power cycle your ECU and wait for a few seconds for the broadcast to be
@@ -13,14 +13,14 @@ from doipclient import DoIPClient
 # ~ print("logical_address" , logical_address)
 # ~ print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>")
 
-# ~ print("get_entity")
-# ~ address, announcement = DoIPClient.get_entity()
-# ~ logical_address = announcement.logical_address
-# ~ ip, port = address
-# ~ print("ip" , ip)
-# ~ print("port" , port)
-# ~ print("logical_address" , logical_address)
+print("get_entity")
+address, announcement = DoIPClient.get_entity()
+logical_address = announcement.logical_address
+ip, port = address
+print("ip" , ip)
+print("port" , port)
+print("logical_address" , logical_address)
 
-print("routing activation")
-client = DoIPClient("10.0.2.15", 3614)
-print(client.request_entity_status())
+# ~ print("routing activation")
+# ~ client = DoIPClient("10.0.2.15", 3614)
+# ~ print(client.request_entity_status())
