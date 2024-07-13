7c0365743ef989679b0d7b2c62dfcb8a4ee18688
Sabeur DAMAK <sabeur.damak@continental-corporation.com>
2024-07-14 01:22:22 +0200

    add lookuptable

---
+++ build/SoAD/CMakeFiles/SoAD.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SoAD.dir/src/soad.c.o"
+  "libSoAD.pdb"
+  "libSoAD.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/SoAD.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/example/CMakeFiles/example.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/uic56995/DoIP_Linux/example/example.c" "/home/uic56995/DoIP_Linux/build/example/CMakeFiles/example.dir/example.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../doip/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/uic56995/DoIP_Linux/build/doip/CMakeFiles/doip.dir/DependInfo.cmake"
+  "/home/uic56995/DoIP_Linux/build/SoAD/CMakeFiles/SoAD.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
